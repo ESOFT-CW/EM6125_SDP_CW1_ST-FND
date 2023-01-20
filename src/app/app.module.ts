e1cbd5336dd5146e94e1c253d16adf4f84ee5940
sanuabeysekara <sanuabeysekara@gmail.com>
2023-01-20 23:46:25 +0530

    Instanced Commit of the Fuel Station Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StationStockComponent } from './station-stock/station-stock.component';
+import { StationDasboardComponent } from './station-dasboard/station-dasboard.component';
+import { StationRequestComponent } from './station-request/station-request.component';
+import { StationApproveComponent } from './station-approve/station-approve.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StationStockComponent,
+    StationDasboardComponent,
+    StationRequestComponent,
+    StationApproveComponent
   ],
   imports: [
     BrowserModule,
